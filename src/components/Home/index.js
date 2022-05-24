1caca3c6f20a4e30c284aaf59ed674d089a1e694
SahuUtkarsh03 <sahuutkarsh03@gmail.com>
2022-05-24 20:01:28 +0530

    Add Animated Letters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
 import './index.scss';
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <React.Fragment>
-            Hello
-        </React.Fragment>
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi <br/> I'm Utkarsh
+                    <img src={LogoTitle} alt="Developer" />ahu
+                    <br/>
+                    Web Developer
+                </h1>
+                <h2>Full Stack Developer / Data Science Enthusiast</h2>
+
+                <Link to="/contact" className="flat-button" > Contact Me </Link>
+            </div>
+        </div>
     );
 }
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,18 +1,68 @@
-import React from 'react';
 import './index.scss';
 import { Link, NavLink } from 'react-router-dom';
 
 import LogoS from '../../assets/images/logo-s.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope,faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub , faTwitter} from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => {
     return (
         <div className='nav-bar'>
+
             <Link className='logo' to='/' >
-                <img src={ LogoS } alt="Small Logo" /> 
+                <img src={LogoS} alt="Small Logo" />
             </Link>
-            <NavLink exact="true" activeclassname="active" to="/" >
-                
-            </NavLink>
+
+            <nav>
+
+                <NavLink exact="true" activeclassname="active" to="/" >
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about" >
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" >
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+
+            </nav>
+
+            <ul>
+
+                <li>
+                    <a
+                        href="https://www.linkedin.com/in/utkarsh-sahu-he-him-5b21541b2/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        href="https://github.com/SahuUtkarsh03"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        href="https://twitter.com/utkarsh4tech"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+                    </a>
+                </li>
+
+            </ul>
         </div>
     );
 }
